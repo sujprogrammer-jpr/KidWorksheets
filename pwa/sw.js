570d85c9dfbe255de4b89e6fa2d89c4329ebb714
sujprogrammer-jpr <sujprogrammer.jpr@gmail.com>
2026-08-08 21:46:59 +0530

    Use display-mode standalone media query for perfect dual-mode browser and installed PWA header layout

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,7 +1,7 @@
 // KidWorksheets Service Worker v1.0
 // Provides offline caching for all app assets
 
-const CACHE_NAME = 'kidworksheets-v15';
+const CACHE_NAME = 'kidworksheets-v16';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
